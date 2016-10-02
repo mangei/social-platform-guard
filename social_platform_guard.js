c66cab6e5b5e81b0f9964df9d695b87c544beb7f
Manuel Geier <manuel@geier.io>
2016-10-02 20:44:55 +0200

    - use Regex to check for specific texts  - add more post regex entries  - add localization for console and user output  - show hints for hidden entries  - prepare popup for configuration  - hide posts instead of removing them (do be able to show them again; for    performance reasons)  - prepare whitelisting for user names

--- social_platform_guard.js
+++ social_platform_guard.js
@@ -1,44 +1,57 @@
-var DEBUG = false;
+var DEBUG = true;
 var THROTTLE_IN_MS = 3000;
+var LEAVE_HINTS = true;
 
-var postSuffixes = [];
-var postSuffixesByLang = {
-    en: [
-        'liked this.',
-        'was tagged in a photo.',
-        'was tagged in 2 photos.',
-        'was tagged in 3 photos.',
-        'was tagged in 4 photos.',
-        'was tagged in 5 photos.',
-        'was tagged in this.',
-        'reacted to this.',
-        'replied to a comment on this.',
-        'commented on this.',
-        'was mentioned in a post.'
-    ],
-    de: [
-        'gefällt das.',
-        'wurde in einem Foto markiert.',
-        'wurde auf 2 Fotos markiert.',
-        'wurde auf 3 Fotos markiert.',
-        'wurde auf 4 Fotos markiert.',
-        'wurde auf 5 Fotos markiert.',
-        'wurde markiert.',
-        'hat auf einen Kommentar dazu geantwortet.',
-        'hat das kommentiert.',
-        'haben das kommentiert.',
-        'wurde in einem Beitrag erwähnt.'
-    ]
+var messages = {
+	appName: 'Social Platform Guard',
+	en: {
+		iAmWorkingForYou: 'I am working for you!',
+		userPost: 'User Post',
+		sponsoredPost: 'Sponsored Post',
+		showPost: 'Show Post',
+		hidden: 'Hidden',
+		postRegexList: [
+			/(.*) liked this\./,
+			/(.*) was tagged in a photo\./,
+			/(.*) was tagged in \d+ photos\./,
+			/(.*) was tagged in this\./,
+			/(.*) reacted to this\./,
+			/(.*) replied to a comment on this\./,
+			/(.*) commented on this\./,
+			/(.*) was mentioned in a post\./,
+			/(.*) replied to a comment on a post from (.*)\./,
+			/(.*) likes (.*)\./,
+			/(.*) liked this post from (.*)\./,
+			/Saved on (.*)/
+		]
+	},
+	de: {
+		iAmWorkingForYou: 'Ich arbeite für dich!',
+		userPost: 'Benutzerbeitrag',
+		sponsoredPost: 'Werbebeitrag',
+		showPost: 'Zeige Beitrag',
+		hidden: 'Ausgeblendet',
+		postRegexList: [
+			/(.*) gefällt das\./,
+			/(.*) wurde in einem Foto markiert\./,
+			/(.*) wurde auf \d+ Fotos markiert\./,
+			/(.*) wurde markiert\./,
+			/(.*) hat darauf reagiert\./,
+			/(.*) hat auf einen Kommentar dazu geantwortet\./,
+			/(.*) hat das kommentiert\./,
+			/(.*) haben das kommentiert\./,
+			/(.*) wurde in einem Beitrag erwähnt\./,
+			// TODO /(.*) replied to a comment on a post from (.*)\./,
+			/(.*) gefällt (.*)\./,
+			// TODO /(.*) liked this post from (.*)\./,
+			/Gespeichert am (.*)/,
+			/(.*) hat diesen Beitrag (.*) markiert./, // XYZ hat diesen Beitrag (September 2014) mit „Gefällt mir“ markiert.,
+		]
+	}
 };
 
 function init() {
-    log('I am working for you!');
-
-    // set language related post suffixes
-    var lang = document.documentElement.lang;
-    if (postSuffixesByLang[lang] !== undefined) {
-        postSuffixes = postSuffixesByLang[lang];
-    }
+    log(getLocalizedMessages().iAmWorkingForYou);
 
     window.addEventListener('scroll', doJob);
     window.onhashchange = function () {
@@ -53,24 +66,33 @@ var doJob = _.throttle(function () {
         var startTime = window.performance.now();
     }
 
-    // remove sponsored posts
-    $('.uiStreamSponsoredLink').each(function () {
-        log('Removed: Sponsored Post');
-        removeElement(getHyperfeedElement($(this)));
+    // hide sponsored posts
+    $('.uiStreamSponsoredLink:not(.guard-checked)').each(function () {
+        $(this).addClass('guard-checked');
+        hideElement(getHyperfeedElement($(this)), getLocalizedMessages().sponsoredPost);
     });
 
+	// hide special posts
     $('.profileLink:not(.guard-checked)').each(function () {
         $(this).addClass('guard-checked');
 
         var text = $(this).parent().parent().text();
+		var user = undefined;
 
-        var removePost = _.some(postSuffixes, function (suffix) {
-            return textEndsWith(text, suffix);
+		var postRegexList = getLocalizedMessages().postRegexList;
+        var shouldHidePost = _.some(postRegexList, function (postRegex) {
+			var matched = postRegex.test(text);
+			if(matched) {
+				var match = postRegex.exec(text);
+				user = match[1];
+			}
+            return matched;
         });
+		
+		var isOnWhiteList = isWhitelisted(user);
 
-        if (removePost) {
-            log('Removed: User Post: ', text);
-            removeElement(getHyperfeedElement($(this)));
+        if (shouldHidePost && !isOnWhiteList) {
+			hideElement(getHyperfeedElement($(this)), getLocalizedMessages().userPost + ': ' + text, user);
         }
     });
 
@@ -84,20 +106,49 @@ function getHyperfeedElement($element) {
     return $element.closest("[id ^= 'hyperfeed_story_id_']");
 }
 
-function textEndsWith(text, suffix) {
-    return text.substr(-suffix.length) === suffix;
-}
-
 function log() {
     var args = Array.prototype.slice.call(arguments);
-    args = ['Social Platform Guard: '].concat(args);
+    args = [messages.appName + ': '].concat(args);
     console.log.apply(this, args);
 }
 
-function removeElement(element) {
-    window.setTimeout(function () {
-        element.remove();
-    }, 0);
+function getLocalizedMessages() {
+	if(getLocalizedMessages.lang === undefined) {
+		getLocalizedMessages.lang = document.documentElement.lang;
+	}
+	return messages[getLocalizedMessages.lang];
 }
+
+function hideElement(element, message, user) {
+    log(getLocalizedMessages().hidden + ': ' + message);
+	
+	if(LEAVE_HINTS) {
+		var showPostElement = document.createElement('p');
+		showPostElement.innerHTML = message;
+		showPostElement.onclick = function() {
+			element.show('slow');
+			showPostElement.remove();
+		};
+		showPostElement.title = getLocalizedMessages().showPost;
+		showPostElement.style['text-align'] = 'center';
+		showPostElement.style['color'] = '#acacac';
+		showPostElement.style['border'] = '1px dashed rgb(168, 168, 168)';
+		showPostElement.style['cursor'] = 'pointer';
+		
+		$(showPostElement).insertBefore(element);
+	}
+	
+    element.hide();
+}
+
+function isWhitelisted(user) {
+	return false; // TODO user === 'A A';
+}
+// TODO users
+// A
+// A A
+// A A A
+// A A and B B
+// A A, B B and C C
 
 init();
